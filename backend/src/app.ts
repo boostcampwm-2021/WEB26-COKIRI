e186512c88dcc1942a01540c92244e655b51f6a1
Hyunmin Jung <jung@hyunmin.dev>
2021-11-01 23:10:51 +0900

    Chore: 프로젝트 셋업

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as logger from 'morgan';
+
+const app: express.Application = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+export default app;
