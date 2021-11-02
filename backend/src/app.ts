bf62e6a128d55bfe51ad64976a48b6f95eb1efe3
Hyunmin Jung <jung@hyunmin.dev>
2021-11-02 15:54:57 +0900

    Chore: 백엔드 설정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app: express.Application = express();
 app.use(logger('dev'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 export default app;
