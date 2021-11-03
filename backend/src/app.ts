c6cc92909b9804b9c22d80c625dbf7d76298de4a
coding_egg <yjh9360@naver.com>
2021-11-04 03:47:40 +0900

    Feat: 백엔드 dotenv 적용

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as loaders from 'src/loaders';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
 
 const app: express.Application = express();
 
