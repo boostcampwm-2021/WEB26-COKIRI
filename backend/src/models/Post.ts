a7be24eb30f3963bf7648f2fabf837e964202618
Hyunmin Jung <jung@hyunmin.dev>
2021-11-04 17:49:49 +0900

    Feat: Post스키마 작성 및 모델 생성

--- backend/src/models/Post.ts
+++ backend/src/models/Post.ts
@@ -1,11 +1,78 @@
-import { Schema, model } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 
-interface Post {
+interface Comment extends Document {
+  user: Schema.Types.ObjectId;
+  content: string;
+}
+interface Like extends Document {
+  user: Schema.Types.ObjectId;
+}
+interface Post extends Document {
   title: string;
+  content: string;
+  versionKey: boolean;
+  user: Schema.Types.ObjectId;
+  image: string;
+  comments: Comment[];
+  likes: Like[];
+  tags: Schema.Types.ObjectId[];
 }
 
-const postSchema = new Schema<Post>({
-  title: { type: String, required: true },
-});
+const commentSchema = new Schema<Comment>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const likeSchema = new Schema<Like>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const postSchema = new Schema<Post>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    image: {
+      type: String,
+    },
+    comments: {
+      type: [commentSchema],
+      default: [],
+    },
+    likes: {
+      type: [likeSchema],
+      default: [],
+    },
+    tags: {
+      type: [Schema.Types.ObjectId],
+      default: [],
+    },
+    versionKey: false,
+  },
+  { timestamps: true },
+);
 
 export default model<Post>('Post', postSchema);
