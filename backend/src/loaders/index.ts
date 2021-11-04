c47430d0ac1d20cb7295ef0b55ce240fafecee5b
Dongmin Kim <kdmin0211@gmail.com>
2021-11-04 18:07:14 +0900

    Feat: passport loader 함수 작성

--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -3,9 +3,11 @@ import expressLoader from 'src/loaders/expressLoader';
 import mongooseLoader from 'src/loaders/mongooseLoader';
 import routerLoader from 'src/loaders/routerLoader';
 import 'src/loaders/passportLoader';
+import passportLoader from 'src/loaders/passportLoader';
 
 export default function init(app: express.Application) {
   expressLoader(app);
   mongooseLoader();
   routerLoader(app);
+  passportLoader(app);
 }
--- backend/src/loaders/passportLoader.ts
+++ backend/src/loaders/passportLoader.ts
@@ -1,27 +1,43 @@
+import * as express from 'express';
 import * as passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
 import { User } from 'src/types';
 
-const jwtOptions: StrategyOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
-};
+export default function passportLoader(app: express.Application): void {
+  const jwtOptions: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
 
-const verifyUser = (jwtPayload: User, done: Function): void => {
-  try {
-    const user: User | undefined = {
-      userID: jwtPayload.userID,
-      username: jwtPayload.username,
-    };
-    if (!user) {
-      done(null, user);
-    } else {
-      done(null, false);
+  const verifyUser = (jwtPayload: User, done: Function): void => {
+    try {
+      const user: User | undefined = {
+        userID: jwtPayload.userID,
+        username: jwtPayload.username,
+      };
+      if (!user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    } catch (error) {
+      done(error, false);
     }
-  } catch (error) {
-    done(error, false);
-  }
-};
+  };
+  passport.use(new JwtStrategy(jwtOptions, verifyUser));
+  passport.initialize();
 
-passport.use(new JwtStrategy(jwtOptions, verifyUser));
-passport.initialize();
+  const authenticateJwt = (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    passport.authenticate('jwt', { session: false }, (error, user) => {
+      if (user) {
+        req.user = user;
+      }
+      next();
+    })(req, res, next);
+  };
+  app.use(authenticateJwt);
+}
