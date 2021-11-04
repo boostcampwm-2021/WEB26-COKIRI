374ec6706baed3ad381af47ce4f1a4084a6e6e50
coding_egg <yjh9360@naver.com>
2021-11-04 11:03:46 +0900

    Refactor: 리뷰 반영
    express loader func 네이밍 변경

--- backend/src/loaders/expressLoader.ts
+++ backend/src/loaders/expressLoader.ts
@@ -2,9 +2,9 @@ import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as logger from 'morgan';
 
-export default (app: express.Application): void => {
+export default function expressLoader(app: express.Application): void {
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-};
+}
