769ad878a05836682eb259b275f1c1309e746537
beomseok han <bmtoss@naver.com>
2021-11-04 22:21:58 +0900

    Feat: echo index페이지 추가

---
+++ frontend/src/pages/echo/index.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react';
+import Head from 'next/head';
+
+function Echo(): ReactElement {
+  return (
+    <div>
+      <Head>
+        <title>COCOO</title>
+        <meta
+          name='description'
+          content='COCOO 사용자들에게 메시지를 보낼 수 있습니다. COCOO에서 대화를 나누어 보세요.'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>Echo home</main>
+
+      <footer />
+    </div>
+  );
+}
+
+export default Echo;
