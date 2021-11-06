0468f9a54852bad79a10d1925ae5a053430a1cae
Dongmin Kim <kdmin0211@gmail.com>
2021-11-06 17:24:25 +0900

    Chore: router loader 추가

--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import expressLoader from 'src/loaders/expressLoader';
 import mongooseLoader from 'src/loaders/mongooseLoader';
 
-export function init(app: express.Application) {
+export default function init(app: express.Application) {
   expressLoader(app);
   mongooseLoader();
 }
