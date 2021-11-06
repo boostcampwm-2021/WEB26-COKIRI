a18dd38955cda816efc70e6f6bfe173e8c2b7d5b
Hyunmin Jung <jung@hyunmin.dev>
2021-11-07 00:58:46 +0900

    Chore: RecoilRoot 세팅

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider, Global } from '@emotion/react';
+import { RecoilRoot } from 'recoil';
 
 import { theme, global } from 'src/styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={global} />
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
