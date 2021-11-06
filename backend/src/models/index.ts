3d377e5fdf559089fbdf552fb685e2d14f79c424
coding_egg <yjh9360@naver.com>
2021-11-07 00:45:08 +0900

    Refactor: model export 변경
    깔끔하게 변경

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,4 +1,3 @@
-import Post from './Post';
-import User from './User';
-
-export { Post, User };
+export { default as Post } from './Post';
+export { default as User } from './User';
+export { default as EchoRoom } from './EchoRoom';
