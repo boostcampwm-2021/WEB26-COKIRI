ef0bdcb684409fac287dfbcf6301073677fa66e4
Hyunmin Jung <jung@hyunmin.dev>
2021-11-06 21:12:55 +0900

    feat: 이모션 테마 기능 사용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@emotion/react';
+
+import theme from 'src/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
