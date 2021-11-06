73f02640ad11d30b970b75916b782cb35e6dc4bc
Hyunmin Jung <jung@hyunmin.dev>
2021-11-06 21:26:07 +0900

    Style: 글로벌 스타일 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider, Global } from '@emotion/react';
 
-import theme from 'src/styles/theme';
+import { theme, global } from 'src/styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={global} />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </ThemeProvider>
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export default css`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
