1db850fe602d64322b50a6f6db97c27321cce198
Hyunmin Jung <jung@hyunmin.dev>
2021-11-06 22:44:11 +0900

    Feat: 유저 추천 카드 홈에서 사용

---
+++ frontend/src/pages/home/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  margin-top: 48px;
+  border-radius: 50px;
+`;
+
+// eslint-disable-next-line import/prefer-default-export
+export { Main };
