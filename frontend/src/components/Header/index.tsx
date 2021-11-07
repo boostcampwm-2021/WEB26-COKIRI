802a6fa4f7012015e03c323fd96ddaca832e11d8
Hyunmin Jung <jung@hyunmin.dev>
2021-11-07 23:50:46 +0900

    Refactor: 선언적 스타일링 적용 및 리팩토링

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,54 +1,51 @@
-import { ReactElement } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import { BiHomeAlt, BiSearch } from 'react-icons/bi';
 import { ImCompass2 } from 'react-icons/im';
 import { FaPaperPlane, FaHeart } from 'react-icons/fa';
 import { IoPersonCircle } from 'react-icons/io5';
-import HeaderStyle from './style';
 
-function Header(): ReactElement {
+import { Row } from 'src/components/Grid';
+import NavigateButton from 'src/components/buttons/NavigateButton';
+import LogoButton from 'src/components/buttons/LogoButton';
+
+import { Wrapper, Section } from './style';
+
+function Header() {
   return (
-    <HeaderStyle>
-      <div className='home-container'>
-        <Link href='/home'>
+    <Wrapper>
+      <Row>
+        <Section>
+          <LogoButton href='/home'>
+            <Image src='/images/logo.svg' width='64' height='24' />
+          </LogoButton>
+          <div className='input-container'>
+            <input type='text' placeholder='search' />
+            <BiSearch size='20' />
+          </div>
+        </Section>
+
+        <Section>
+          <NavigateButton href='/home'>
+            <BiHomeAlt />
+          </NavigateButton>
+          <NavigateButton href='/random'>
+            <ImCompass2 />
+          </NavigateButton>
+          <NavigateButton href='/echo'>
+            <FaPaperPlane />
+          </NavigateButton>
+        </Section>
+
+        <Section>
           <button type='button'>
-            <BiHomeAlt size='30' />
+            <FaHeart size='25' />
           </button>
-        </Link>
-        <Link href='/random'>
-          <button type='button'>
-            <ImCompass2 size='25' />
-          </button>
-        </Link>
-        <Link href='/echo/echoID'>
-          <button type='button'>
-            <FaPaperPlane size='20' />
-          </button>
-        </Link>
-      </div>
-      <div className='logo-container'>
-        <Link href='/home'>
-          <button type='button'>
-            <Image src='/images/logo.png' width='110px' height='30px' />
-          </button>
-        </Link>
-        <div className='input-container'>
-          <input type='text' placeholder='search' />
-          <BiSearch size='20' />
-        </div>
-      </div>
-      <div className='user-container'>
-        <button type='button'>
-          <FaHeart size='25' />
-        </button>
-        <Link href='/users/tiger'>
-          <button type='button'>
+          <NavigateButton href='/users/tiger'>
             <IoPersonCircle size='30' />
-          </button>
-        </Link>
-      </div>
-    </HeaderStyle>
+          </NavigateButton>
+        </Section>
+      </Row>
+    </Wrapper>
   );
 }
 
--- frontend/src/components/Header/style.ts
+++ frontend/src/components/Header/style.ts
@@ -1,77 +1,22 @@
 import styled from '@emotion/styled';
 
-const HeaderStyle = styled.div`
-  * {
-    background: #555;
-  }
-  background: #555;
-  position: fixed;
+const Wrapper = styled.header`
   top: 0;
-  display: flex;
-  height: 50px;
-  align-items: center;
-  width: 100%;
-  button {
-    border: 0px;
-    background-color: rgbx(255, 255, 255, 0);
-  }
-  button:hover {
-    cursor: pointer;
-  }
-  .logo-container {
-    position: fixed;
-    top: 10px;
-    left: 15%;
-    display: flex;
-    align-items: center;
-    .input-container {
-      margin-left: 20px;
-      border-radius: 10 px;
-      align-items: center;
-      display: flex;
-      background: #666;
-      border-radius: 3px;
-      padding: 0px 5px;
-      input {
-        margin-left: 3px;
-        border: none;
-        background: #666;
-        font-size: 16px;
-        color: white;
-        width: 150px;
-      }
-      input:focus {
-        color: white;
-        outline: none;
-      }
-      svg {
-        background: #666;
-      }
-    }
-  }
-  .home-container {
-    position: fixed;
-    top: 10px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    button {
-      margin: 0px 10px;
-    }
-    button:last-child {
-      margin-left: 12px;
-    }
-  }
-  .user-container {
-    position: fixed;
-    top: 10px;
-    display: flex;
-    right: 15%;
-    align-items: center;
-    button {
-      margin: 0px 3px;
-    }
+  left: 0;
+  right: 0;
+  position: fixed;
+
+  *,
+  & {
+    background-color: transparent;
   }
 `;
 
-export default HeaderStyle;
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  align-items: center;
+`;
+
+export { Wrapper, Section };
