c5cc5bd459dfbd5e48e8bdb1cb4efe67a21269a5
Hyunmin Jung <jung@hyunmin.dev>
2021-11-07 23:51:25 +0900

    Feat: SearchBox 컴포넌트 프로토타입 제작

---
+++ frontend/src/components/SearchBox/index.tsx
@@ -0,0 +1,12 @@
+import { BiSearch } from 'react-icons/bi';
+
+function SearchBox() {
+  return (
+    <>
+      <input type='text' placeholder='search' />
+      <BiSearch size='20' />
+    </>
+  );
+}
+
+export default SearchBox;
