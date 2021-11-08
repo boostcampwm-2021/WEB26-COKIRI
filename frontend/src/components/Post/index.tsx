e5b07a2a5c711a9e18fb8ebb5f6076e3826b6c5c
beomseok han <bmtoss@naver.com>
2021-11-08 22:24:06 +0900

    Feat: timeline, post component 구조 잡기

---
+++ frontend/src/components/Post/index.tsx
@@ -0,0 +1,38 @@
+import { IoHeartOutline, IoPaperPlaneOutline, IoChatbubbleOutline } from 'react-icons/io5';
+
+import ProfileImageButton from 'src/components/buttons/ProfileImageButton';
+import CommentButton from 'src/components/buttons/post/CommentButton';
+import LikeButton from 'src/components/buttons/post/LikeButton';
+import EchoButton from 'src/components/buttons/post/EchoButton';
+import PostImage from 'src/components/Post/PostImage';
+import PostLikeRow from 'src/components/Post/PostLikeRow';
+import PostContent from 'src/components/Post/PostContent';
+import PostReview from 'src/components/Post/PostReview';
+
+import { IconRow } from './style';
+
+function Post() {
+  return (
+    <>
+      <ProfileImageButton href='users/123'>image, username</ProfileImageButton>
+      <PostImage />
+      <IconRow>
+        <CommentButton>
+          <IoHeartOutline />
+        </CommentButton>
+        <LikeButton>
+          <IoChatbubbleOutline />
+        </LikeButton>
+
+        <EchoButton href='echo/123'>
+          <IoPaperPlaneOutline />
+        </EchoButton>
+      </IconRow>
+      <PostLikeRow />
+      <PostContent />
+      <PostReview />
+    </>
+  );
+}
+
+export default Post;
---
+++ frontend/src/components/Post/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const IconRow = styled.div`
+  display: flex;
+  margin-left: 5px;
+  svg {
+    margin: 5px;
+  }
+`;
+// eslint-disable-next-line import/prefer-default-export
+export { IconRow };
