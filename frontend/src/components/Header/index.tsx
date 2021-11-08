1dfbc5ec03a93685570e41986dc0a816a930497d
beomseok han <bmtoss@naver.com>
2021-11-08 16:03:31 +0900

    Feat: IconButton component 구현

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image';
-import { IoHome, IoCompass, IoPaperPlaneOutline, IoHeart, IoPersonCircle } from 'react-icons/io5';
+import {
+  IoHome,
+  IoCompassSharp,
+  IoPaperPlaneOutline,
+  IoHeart,
+  IoPersonCircle,
+} from 'react-icons/io5';
 
 import { Row } from 'src/components/Grid';
 import NavigateButton from 'src/components/buttons/NavigateButton';
 import LogoButton from 'src/components/buttons/LogoButton';
+import IconButton from 'src/components/buttons/IconButton';
 import SearchBox from 'src/components/SearchBox';
 
 import { Wrapper, Section } from './style';
@@ -24,7 +31,7 @@ function Header() {
             <IoHome />
           </NavigateButton>
           <NavigateButton href='/random'>
-            <IoCompass />
+            <IoCompassSharp />
           </NavigateButton>
           <NavigateButton href='/echo'>
             <IoPaperPlaneOutline />
@@ -32,9 +39,9 @@ function Header() {
         </Section>
 
         <Section>
-          <button type='button'>
-            <IoHeart size='25' />
-          </button>
+          <IconButton>
+            <IoHeart />
+          </IconButton>
           <NavigateButton href='/users/tiger'>
             <IoPersonCircle />
           </NavigateButton>
