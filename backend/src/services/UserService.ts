f5856b457dc6530dadfbeb5d928af7039ba2c682
Dongmin Kim <kdmin0211@gmail.com>
2021-11-09 16:51:07 +0900

    Feat: 유저 정보 업데이트 block list 반영

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,6 +1,6 @@
 import { nanoid } from 'nanoid';
 
-import { User, Language } from 'src/models';
+import { User } from 'src/models';
 import { User as UserType, UserAuthProvider } from 'src/types';
 import { UserType as UserSchemaType } from 'src/types/modelType';
 import { ObjectID } from 'src/utils';
@@ -71,7 +71,14 @@ class UserService {
     return result[0];
   }
 
-  static async updateOneUserConfig(user: UserType, userConfig: UserSchemaType) {
+  static async updateOneUserConfig<Type extends { [key: string]: any }>(
+    user: UserType,
+    userConfig: Type,
+  ) {
+    const blockList = ['followers', 'follows', 'posts', 'likes', 'notifies', 'dashboard'];
+    blockList.forEach((property: string) => {
+      if (userConfig[property]) throw new Error('잘못된 요청입니다.');
+    });
     await User.updateOne({ _id: user.userID }, userConfig, { runValidators: true });
   }
 }
