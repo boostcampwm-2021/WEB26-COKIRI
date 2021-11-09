e33941ee73df88f5d3c0209679c22403e40652c7
beomseok han <bmtoss@naver.com>
2021-11-09 18:28:18 +0900

    Feat: 슬라이드 버튼 컴포넌트 구현
    - src/components/buttons/post/slide

---
+++ frontend/src/components/buttons/post/slide/Common/index.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-use-before-define
+import React, { ReactNode } from 'react';
+import PropTypes from 'prop-types';
+import { IconContext } from 'react-icons';
+
+import { Button } from './style';
+
+interface Props {
+  children: ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function SlideButton({ children, onClick }: Props) {
+  return (
+    <Button onClick={onClick}>
+      <IconContext.Provider value={{ size: '28px' }}>{children}</IconContext.Provider>
+    </Button>
+  );
+}
+
+SlideButton.propsType = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default SlideButton;
