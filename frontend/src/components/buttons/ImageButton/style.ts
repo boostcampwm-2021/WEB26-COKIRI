480a9acc53e738007614a8be4f182b10c0e5dc8b
beomseok han <bmtoss@naver.com>
2021-11-09 11:24:25 +0900

    Style: 버튼 꽉 차게 스타일 적용
    - header icon button 꽉 차게 수정

--- frontend/src/components/buttons/ImageButton/style.ts
+++ frontend/src/components/buttons/ImageButton/style.ts
@@ -1,9 +1,6 @@
 import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   margin: 8px;
   width: 144px;
   height: 48px;
@@ -11,7 +8,11 @@ const Wrapper = styled.div`
   background: #444444;
   box-shadow: 5px 5px 10px #3a3a3a, -5px -5px 10px #4e4e4e;
   a {
-    height: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
   }
 `;
 
