084eb4ebfa8125e081aa069e890c3e929022d53a
Hyunmin Jung <jung@hyunmin.dev>
2021-11-09 14:27:31 +0900

    Feat: fetcher 모듈에 getUsersMe 스태틱 함수 구현

--- frontend/src/utils/Fetcher.ts
+++ frontend/src/utils/Fetcher.ts
@@ -1,13 +1,24 @@
 import axios, { AxiosInstance } from 'axios';
 
+import { UserType } from 'src/types';
+
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   headers: {},
 });
 
 class Fetcher {
-  static async getPosts() {
-    await axiosInstance.get('/posts');
+  static async getUsersMe(token: string): Promise<UserType> {
+    try {
+      const result = await axiosInstance.get('/v1/users/me', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return result.data.user;
+    } catch (error) {
+      return {};
+    }
   }
 }
 
