f29b08f8cc65d01429ec1776b814d86e99c9d3c2
Hyunmin Jung <jung@hyunmin.dev>
2021-11-09 20:12:51 +0900

    Feat: 회원가입 모달 기능 완성

--- frontend/src/components/modals/RegisterModal/index.tsx
+++ frontend/src/components/modals/RegisterModal/index.tsx
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
 import { useMutation } from 'react-query';
+import { useRecoilValue } from 'recoil';
+import PropTypes from 'prop-types';
 
 import Modal from 'src/components/modals/Common';
 import { Col } from 'src/components/Grid';
 
 import { Fetcher } from 'src/utils';
-import userAtom from 'src/recoil/user';
-import { useRecoilValue } from 'recoil';
 
-function RegisterModal() {
+import userAtom from 'src/recoil/user';
+
+interface Props {
+  onClose: () => void;
+}
+
+function RegisterModal({ onClose }: Props) {
   const [username, setUsername] = useState('');
   const user = useRecoilValue(userAtom);
-  const mutation = useMutation(() => Fetcher.setUsername(username, user));
-
-  const handleOnClose = () => {};
-
-  const handleOnConfirm = () => {
-    mutation.mutate();
-  };
-
+  const putUsersUsername = () => Fetcher.putUsersUsername(username, user);
+  const mutation = useMutation(putUsersUsername, {
+    onSuccess: () => onClose(),
+  });
+  const handleOnConfirm = () => mutation.mutate();
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
   };
-
   return (
-    <Modal onConfirm={handleOnConfirm} onClose={handleOnClose} close='로그아웃' confirm='확인'>
+    <Modal onConfirm={handleOnConfirm} close='로그아웃' confirm='확인'>
       <Col>
         <p>회원가입에 필요한 절차에요</p>
         <p>username을 알려주세요~</p>
@@ -34,5 +36,13 @@ function RegisterModal() {
     </Modal>
   );
 }
+
+RegisterModal.propTypes = {
+  onClose: PropTypes.func,
+};
+
+RegisterModal.defaultProps = {
+  onClose: () => {},
+};
 
 export default RegisterModal;
