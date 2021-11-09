db107e2b4cfcbf33a96674fd8484cc9afa60aeb0
Hyunmin Jung <jung@hyunmin.dev>
2021-11-10 00:52:25 +0900

    Feat: 플로팅 버튼 제작

--- frontend/src/components/buttons/FloatingButton/index.tsx
+++ frontend/src/components/buttons/FloatingButton/index.tsx
@@ -1,7 +1,26 @@
-import { Wrapper } from './style';
+import { useCallback, useState } from 'react';
+
+import PostModal from 'src/components/modals/PostModal';
+
+import { Wrapper, Button } from './style';
 
 function FloatingButton() {
-  return <Wrapper>test</Wrapper>;
+  const [isModalShow, setIsModalShow] = useState(false);
+
+  const changeModalShow = useCallback(() => {
+    setIsModalShow((prevState) => !prevState);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setIsModalShow(false);
+  }, []);
+
+  return (
+    <Wrapper>
+      {isModalShow && <PostModal onClose={handleClose} />}
+      <Button onClick={changeModalShow}>작성</Button>
+    </Wrapper>
+  );
 }
 
 export default FloatingButton;
