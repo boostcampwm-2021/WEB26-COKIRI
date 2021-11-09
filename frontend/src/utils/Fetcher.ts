14d5c23e3076533a4020d2cef4f19a76625b931f
Hyunmin Jung <jung@hyunmin.dev>
2021-11-09 20:10:02 +0900

    Refactor: 유저네임 변경 fetcher 이름 변경

--- frontend/src/utils/Fetcher.ts
+++ frontend/src/utils/Fetcher.ts
@@ -25,7 +25,7 @@ class Fetcher {
   }
 
   // for client side
-  static async setUsername(username: string, user: UserType): Promise<any> {
+  static async putUsersUsername(username: string, user: UserType): Promise<any> {
     await axios.put(
       // eslint-disable-next-line no-underscore-dangle
       `${baseURL}/v1/users/${user._id}`,
