f7daafd9cff4e51c58dedef6d40c2912cd35d4b7
Hyunmin Jung <jung@hyunmin.dev>
2021-11-11 00:53:59 +0900

    Feat: Post 컴포넌트에 실제 값 반영

--- frontend/src/components/Post/index.tsx
+++ frontend/src/components/Post/index.tsx
@@ -9,54 +9,52 @@ import EchoButton from 'src/components/buttons/EchoButton';
 import PostImages from 'src/components/PostImages';
 import PostContent from 'src/components/PostContent';
 import PostReview from 'src/components/PostReview';
-import { Row } from 'src/components/Grid';
 import LikeListButton from 'src/components/buttons/LikeListButton';
 import LikeListModal from 'src/components/modals/LikeListModal';
+import Card from 'src/components/cards/Common';
+import { Row } from 'src/components/Grid';
 
 import PostType from 'src/types/post';
 
 import { Wrapper, Buttons } from './style';
 
 interface Props {
-  post?: PostType;
+  post: PostType;
 }
 
 function Post({ post }: Props) {
   const [isLikeListModal, setIsLikeListModal] = useState(false);
   const handleClick = useCallback(() => setIsLikeListModal(true), []);
   const handleClose = useCallback(() => setIsLikeListModal(false), []);
-  const writer = 'tiger';
-  const likeCount = 3;
   return (
     <Wrapper>
-      <Profile href={`users/${writer}`} imageSrc='/images/logo.svg' userName={writer} />
-      <PostImages images={[]} />
-      <Row justifyContent='flex-start'>
-        <Buttons>
-          <CommentButton>
-            <IoHeartOutline />
-          </CommentButton>
-          <LikeButton>
-            <IoChatbubbleOutline />
-          </LikeButton>
-          <EchoButton href='echo/123'>
-            <IoPaperPlaneOutline />
-          </EchoButton>
-        </Buttons>
-      </Row>
-      <LikeListButton length={likeCount} handleClick={openModal} />
-      <PostContent content='hihi' />
-      <PostReview />
+      <Card width={600} height={600}>
+        <Profile imageSrc='/images/logo.svg' username='beomseok' />
+        <PostImages images={post.images} />
+        <Row justifyContent='flex-start'>
+          <Buttons>
+            <CommentButton>
+              <IoHeartOutline />
+            </CommentButton>
+            <LikeButton>
+              <IoChatbubbleOutline />
+            </LikeButton>
+            <EchoButton href='echo/123'>
+              <IoPaperPlaneOutline />
+            </EchoButton>
+          </Buttons>
+        </Row>
+        <LikeListButton length={post.likes.length} onClick={handleClick} />
+        <PostContent content={post.content} />
+        <PostReview />
         {isLikeListModal && <LikeListModal onClose={handleClose} />}
+      </Card>
     </Wrapper>
   );
 }
 
 Post.propTypes = {
-  post: PropTypes.objectOf(PropTypes.any),
+  post: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
-Post.defaultProps = {
-  post: {},
-};
 export default Post;
