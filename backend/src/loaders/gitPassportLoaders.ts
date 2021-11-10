079d0d26dcec38276de64e570d2364869eb9e8ca
coding_egg <yjh9360@naver.com>
2021-11-10 20:13:30 +0900

    Feat: github OAuth 구현

---
+++ backend/src/loaders/gitPassportLoaders.ts
@@ -0,0 +1,36 @@
+import { Application } from 'express';
+import * as passport from 'passport';
+import * as GitHubStrategy from 'passport-github';
+
+import { UserService } from 'src/services';
+
+export default function passportLoader(app: Application): void {
+  const githubStrategyOptions = {
+    clientID: process.env.GITHUB_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_SECRET_ID!,
+    callbackURL: process.env.GITHUB_CALLBACK_URL!,
+  };
+  console.log(githubStrategyOptions);
+
+  const verifyGithubUser = async (
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    cb: any,
+  ) => {
+    try {
+      /* eslint-disable-next-line no-underscore-dangle */
+      const { id, login: username } = profile._json;
+      const user = await UserService.findOrCreateUserForProvider({
+        authProvider: 'github',
+        authProviderID: id,
+      });
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
+  };
+
+  passport.use('github', new GitHubStrategy(githubStrategyOptions, verifyGithubUser));
+  app.use(passport.initialize());
+}
--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import expressLoader from 'src/loaders/expressLoader';
 import mongooseLoader from 'src/loaders/mongooseLoader';
 import passportLoader from 'src/loaders/passportLoader';
+import gitPassportLoaders from 'src/loaders/gitPassportLoaders';
 import routerLoader from 'src/loaders/routerLoader';
 import corsLoader from 'src/loaders/corsLoader';
 
@@ -10,6 +11,7 @@ export default function init(app: express.Application) {
   expressLoader(app);
   mongooseLoader();
   passportLoader(app);
+  gitPassportLoaders(app);
   corsLoader(app);
   routerLoader(app);
 }
