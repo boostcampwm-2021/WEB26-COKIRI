21e85b398522088a8ecfa48b3a84773879174370
Hyunmin Jung <jung@hyunmin.dev>
2021-11-11 23:38:18 +0900

    Fix: 배포를 위한 최종 버그 점검

--- frontend/src/utils/Fetcher.ts
+++ frontend/src/utils/Fetcher.ts
@@ -29,10 +29,15 @@ class Fetcher {
   }
 
   // for client side
-  static async putUsersUsername(username: string, user: UserType): Promise<void> {
+  static async putUserSettings(newUser: UserType, user: UserType): Promise<void> {
     await axios.put(
-      `${baseURL}/v1/users/${user._id}`,
-      { username },
+      `${baseURL}/v1/users/${user._id}/settings`,
+      {
+        username: newUser.username,
+        name: newUser.name,
+        profileImage: newUser.profileImage,
+        bio: newUser.bio,
+      },
       {
         headers: { Authorization: `Bearer ${user.token}` },
       },
@@ -54,7 +59,10 @@ class Fetcher {
   }
 
   static async getPosts(user: UserType): Promise<PostType[]> {
-    const result = await axios.get(`${baseURL}/v1/posts/?userID=${user._id}&offset=${0}`, {
+    if (user._id === undefined) {
+      return [];
+    }
+    const result = await axios.get(`${baseURL}/v1/posts/?user_id=${user._id}&offset=${0}`, {
       headers: { Authorization: `Bearer ${user.token}` },
     });
     return result.data;
