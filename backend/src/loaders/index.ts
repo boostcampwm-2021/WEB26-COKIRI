3df59193bc05c389f14ee930a60847f9b4db998e
Dongmin Kim <kdmin0211@gmail.com>
2021-11-11 17:52:11 +0900

    Refactor: loader 이름 변경

--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -5,7 +5,7 @@ import mongooseLoader from 'src/loaders/mongooseLoader';
 import passportLoader from 'src/loaders/passportLoader';
 import passportJWTLoader from 'src/loaders/passportJWTLoader';
 import passportGoogleLoader from 'src/loaders/passportGoogleLoader';
-import gitPassportLoaders from 'src/loaders/gitPassportLoaders';
+import passportGithubLoader from 'src/loaders/passportGithubLoader';
 import routerLoader from 'src/loaders/routerLoader';
 import corsLoader from 'src/loaders/corsLoader';
 
@@ -15,7 +15,7 @@ export default function init(app: express.Application) {
   passportLoader(app);
   passportJWTLoader();
   passportGoogleLoader();
-  gitPassportLoaders(app);
+  passportGithubLoader();
   corsLoader(app);
   routerLoader(app);
 }
