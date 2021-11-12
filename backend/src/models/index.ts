5892190da996b678ef41f6f53cc8b83d8014a4a1
coding_egg <yjh9360@naver.com>
2021-11-13 03:09:18 +0900

    Feat: models 인덱스 export 추가

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,10 +1,18 @@
 import { Validate } from 'src/utils';
-import Post from './Post';
-import User from './User';
+import Comment from './Comment';
+import CommentLike from './CommentLike';
+import DashboardHistory from './DashboardHistory';
+import DashboardPost from './DashboardPost';
+import EchoMessage from './EchoMessage';
 import EchoRoom from './EchoRoom';
-import Tag from './Tag';
+import Follow from './Follow';
+import Image from './Image';
 import Language from './Language';
 import Notify from './Notify';
+import Post from './Post';
+import Postlike from './Postlike';
+import Tag from './Tag';
+import User from './User';
 
 Validate.UserModel = User;
 Validate.PostModel = Post;
@@ -13,4 +21,19 @@ Validate.EchoRoomModel = EchoRoom;
 Validate.LanguageModel = Language;
 Validate.NotifyModel = Notify;
 
-export { Post, User, EchoRoom, Tag, Language, Notify };
+export {
+  Comment,
+  CommentLike,
+  DashboardHistory,
+  DashboardPost,
+  EchoMessage,
+  EchoRoom,
+  Follow,
+  Image,
+  Language,
+  Notify,
+  Post,
+  Postlike,
+  Tag,
+  User,
+};
