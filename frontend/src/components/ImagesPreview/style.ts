c4ad226fb21f32ae7df0b6bfdb7430b1af6b3a32
Hyunmin Jung <jung@hyunmin.dev>
2021-11-15 15:57:15 +0900

    Style: ImagePreview width 값 수정

--- frontend/src/components/ImagesPreview/style.ts
+++ frontend/src/components/ImagesPreview/style.ts
@@ -1,6 +1,12 @@
 import styled from '@emotion/styled';
 
+import { Col } from 'src/components/Grid';
+
 const Wrapper = styled.div`
+  ${Col} {
+    width: 120px;
+    margin: 0 4px;
+  }
   img {
     border-radius: 24px;
   }
