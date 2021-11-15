e6c1d5c839f2b9ea1dc9bd404cf52372f9fd10ef
coding_egg <yjh9360@naver.com>
2021-11-15 23:39:42 +0900

    Feat: 에러코드 상수 추가 (쿼리)

--- backend/src/utils/Enums.ts
+++ backend/src/utils/Enums.ts
@@ -23,6 +23,7 @@ const Enums = {
     NO_USERS: '사용자가 존재하지 않습니다.',
     NO_POSTS: '게시글이 존재하지 않습니다.',
     NO_COMMENTS: '댓글이 존재하지 않습니다.',
+    NO_QUERY: '검색 내용이 존재하지 않습니다.',
   },
   openAPIUrl: {
     PROBLEM_SEARCH_SUGGESTION: 'https://solved.ac/api/v3/search/suggestion',
