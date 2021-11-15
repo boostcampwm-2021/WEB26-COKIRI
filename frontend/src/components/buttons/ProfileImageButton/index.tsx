c0018f6a589a0deb0b4d4116ab67235045cb35ef
beomseok han <bmtoss@naver.com>
2021-11-15 23:38:03 +0900

    Chore: 프로필 이미지 버튼 size props 추가

--- frontend/src/components/buttons/ProfileImageButton/index.tsx
+++ frontend/src/components/buttons/ProfileImageButton/index.tsx
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types';
 
 import ProfileImage from 'src/components/images/ProfileImage';
 
-import { DEFAULT_PROFILE_IMAGE } from 'src/globals/constants';
+import { DEFAULT_PROFILE_IMAGE, DEFAULT_PROFILE_IMAGE_SIZE } from 'src/globals/constants';
 
 import { Wrapper } from './style';
 
 interface Props {
   username?: string;
   profileImage?: string;
+  size?: number;
 }
 
-function ProfileImageButton({ username, profileImage }: Props) {
+function ProfileImageButton({ username, profileImage, size }: Props) {
   return (
     <Wrapper>
       <Link href={`/users/${username}`} passHref>
         <a>
-          <ProfileImage profileImage={profileImage!} />
+          <ProfileImage profileImage={profileImage!} size={size} />
         </a>
       </Link>
     </Wrapper>
@@ -27,11 +28,13 @@ function ProfileImageButton({ username, profileImage }: Props) {
 ProfileImageButton.propsType = {
   username: PropTypes.string,
   profileImage: PropTypes.string,
+  size: PropTypes.number,
 };
 
 ProfileImageButton.defaultProps = {
   username: '',
   profileImage: DEFAULT_PROFILE_IMAGE,
+  size: DEFAULT_PROFILE_IMAGE_SIZE,
 };
 
 export default ProfileImageButton;
