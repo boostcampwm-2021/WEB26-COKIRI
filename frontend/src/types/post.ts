73af3c5df799313bf7059a276caeb9a763804cee
beomseok han <bmtoss@naver.com>
2021-11-15 14:55:07 +0900

    Fix: post의 images 타입 수정

--- frontend/src/types/post.ts
+++ frontend/src/types/post.ts
@@ -1,11 +1,12 @@
 import commentType from './comment';
 import LikeType from './like';
 import UserType from './user';
+import ImageType from './image';
 
 export default interface postType {
   _id: string;
   content: string;
-  images: string[];
+  images: ImageType[];
   user: UserType;
   likeCount: number;
   comments: commentType[];
