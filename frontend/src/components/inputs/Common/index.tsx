35803e4647e7ebfe8cf4b728ee5e5dd932ef6cae
Hyunmin Jung <jung@hyunmin.dev>
2021-11-17 17:54:53 +0900

    Feat: CommonInput 더 사용하기 쉽게 변경

--- frontend/src/components/inputs/Common/index.tsx
+++ frontend/src/components/inputs/Common/index.tsx
@@ -1,44 +1,57 @@
-import React, { Dispatch, ReactNode, SetStateAction, useCallback } from 'react';
+import React, { Dispatch, ReactNode, SetStateAction, useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Wrapper, Input } from './style';
 
 interface Props {
-  bind: [string, Dispatch<SetStateAction<string>>];
+  bind?: [string, Dispatch<SetStateAction<string>>];
   placeholder?: string;
   width?: number;
   icon?: ReactNode;
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (state: string) => void;
 }
 
-function InputCommon({ bind, placeholder, width, icon }: Props) {
-  const [state, setState] = bind;
+function InputCommon({ bind, placeholder, width, icon, onChange }: Props) {
+  const [state, setState] = bind!;
+  const [fallbackState, setFallbackState] = useState('');
 
   const handleChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       setState(event.target.value);
+      setFallbackState(event.target.value);
+      onChange!(event.target.value);
     },
-    [setState],
+    [onChange, setState],
   );
 
   return (
     <Wrapper width={width!}>
-      <Input width={width!} value={state} onChange={handleChange} placeholder={placeholder} />
+      <Input
+        width={width!}
+        value={state || fallbackState}
+        onChange={handleChange}
+        placeholder={placeholder}
+      />
       {icon}
     </Wrapper>
   );
 }
 
 InputCommon.propTypes = {
-  bind: PropTypes.arrayOf(PropTypes.any).isRequired,
+  bind: PropTypes.arrayOf(PropTypes.any),
   placeholder: PropTypes.string,
   width: PropTypes.number,
   icon: PropTypes.node,
+  onChange: PropTypes.func,
 };
 
 InputCommon.defaultProps = {
+  bind: ['', () => {}],
   placeholder: '',
   width: 0,
   icon: '',
+  onChange: () => {},
 };
 
 export default InputCommon;
