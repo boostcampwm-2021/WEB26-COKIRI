27f574f225c0b1e57b57e520a90dd13d3ccc696c
beomseok han <bmtoss@naver.com>
2021-11-18 01:17:46 +0900

    Fix: 전체 보기 두번 째 들어갈 때 안 들어가지는 버그 수정

--- frontend/src/components/DetailPost/index.tsx
+++ frontend/src/components/DetailPost/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import PropTypes from 'prop-types';
 
 import PostImages from 'src/components/images/PostImages';
@@ -22,60 +23,77 @@ import {
   DETAIL_PROFILE_SET_MARGIN_LEFT,
 } from 'src/globals/constants';
 
-import { PostType, CommentType } from 'src/types';
+import { CommentType } from 'src/types';
+
+import { Fetcher } from 'src/utils';
 
 import { ImageSection, PostInfoSection } from './style';
 
 interface Props {
-  post: PostType;
+  postID: string | string[];
 }
 
-function DetailPost({ post }: Props) {
-  const [likeCount, setLikeCount] = useState(post.likes?.length ?? 0);
+function DetailPost({ postID }: Props) {
   const [comments, setComments] = useState<CommentType[]>([]);
+  const { isFetched, data: detailPost } = useQuery(['detail', 'posts', postID], () =>
+    Fetcher.getDetailPost(postID),
+  );
   useEffect(() => {
-    setComments(post.comments!);
-  }, [post.comments]);
-  const { _id, images, user, likes, content } = post;
+    setComments(detailPost?.comments ?? []);
+  }, [detailPost]);
+  const [likeCount, setLikeCount] = useState(detailPost?.likes?.length ?? 0);
   return (
-    <Row>
-      <ImageSection>
-        <PostImages
-          images={images!}
-          width={DETAIL_POST_IMAGE_WIDTH}
-          height={DETAIL_POST_IMAGE_HEIGHT}
-        />
-      </ImageSection>
-      <PostInfoSection>
-        <Col>
-          <ProfileSet
-            profileImage={user!.profileImage}
-            username={user!.username!}
-            marginLeft={DETAIL_PROFILE_SET_MARGIN_LEFT}
-          />
-          <Row justifyContent='space-evenly'>
-            <LikeButton postID={_id!} postLikes={likes!} setLikeCount={setLikeCount} />
-            <CommentButton postID={_id!} />
-            <EchoButton postID={_id!} />
-          </Row>
-          {likeCount !== 0 && <LikesButton postID={_id!} likeCount={likeCount} />}
-          <PostContent content={content!} width={DETAIL_POST_CONTENT_WIDTH} />
-          <PostComments postID={_id!} comments={comments} />
-          <CommentInput
-            postID={_id!}
-            setComments={setComments}
-            width={DETAIL_COMMENT_INPUT_WIDTH}
-            iconSize={DETAIL_COMMENT_ICON_SIZE}
-            padding={DETAIL_COMMENT_ICON_PADDING}
-          />
-        </Col>
-      </PostInfoSection>
-    </Row>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {isFetched && (
+        <Row>
+          <ImageSection>
+            <PostImages
+              images={detailPost?.images!}
+              width={DETAIL_POST_IMAGE_WIDTH}
+              height={DETAIL_POST_IMAGE_HEIGHT}
+            />
+          </ImageSection>
+          <PostInfoSection>
+            <Col>
+              <ProfileSet
+                profileImage={detailPost?.user!.profileImage}
+                username={detailPost?.user!.username!}
+                marginLeft={DETAIL_PROFILE_SET_MARGIN_LEFT}
+              />
+              <Row justifyContent='space-evenly'>
+                <LikeButton
+                  postID={detailPost?._id!}
+                  postLikes={detailPost?.likes!}
+                  setLikeCount={setLikeCount}
+                />
+                <CommentButton postID={detailPost?._id!} />
+                <EchoButton postID={detailPost?._id!} />
+              </Row>
+              {likeCount !== 0 && <LikesButton postID={detailPost?._id!} likeCount={likeCount} />}
+              <PostContent
+                content={detailPost?.content!}
+                width={DETAIL_POST_CONTENT_WIDTH}
+                detail
+              />
+              <PostComments postID={detailPost?._id!} comments={comments} detail />
+              <CommentInput
+                postID={detailPost?._id!}
+                setComments={setComments}
+                width={DETAIL_COMMENT_INPUT_WIDTH}
+                iconSize={DETAIL_COMMENT_ICON_SIZE}
+                padding={DETAIL_COMMENT_ICON_PADDING}
+              />
+            </Col>
+          </PostInfoSection>
+        </Row>
+      )}
+    </>
   );
 }
 
 DetailPost.propTypes = {
-  post: PropTypes.objectOf(PropTypes.any).isRequired,
+  postID: PropTypes.string.isRequired,
 };
 
 export default DetailPost;
