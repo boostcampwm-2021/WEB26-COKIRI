bc8012cf00100c949c802ca0057cbed93ec8f873
Dongmin Kim <kdmin0211@gmail.com>
2021-11-21 00:09:48 +0900

    Feat: 유저의 hisotry get 추가

--- backend/src/services/DashboardHistoryService.ts
+++ backend/src/services/DashboardHistoryService.ts
@@ -1,9 +1,15 @@
+import { Types } from 'mongoose';
+
 import { DashboardHistory } from 'src/models';
 
 class DashboardHistoryService {
   async createDashboardHistory(userID: string, content: string, date: string) {
     const history = await DashboardHistory.create({ userID, content, date });
     return history;
+  }
+
+  async findDashboardHistory(userID: string | Types.ObjectId) {
+    return DashboardHistory.find({ userID }).sort({ date: 1 }).lean();
   }
 
   async deleteDashboardHistory(userID: string, historyID: string) {
