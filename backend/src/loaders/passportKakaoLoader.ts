bbfbeeb12aeebfa8823c43d433b80fe903812693
Dongmin Kim <kdmin0211@gmail.com>
2021-11-23 00:02:35 +0900

    Feat: kakao 인증 passport loader 추가

---
+++ backend/src/loaders/passportKakaoLoader.ts
@@ -0,0 +1,27 @@
+import { Strategy as KakaoStrategy } from 'passport-kakao';
+import * as passport from 'passport';
+import { UserService } from 'src/services';
+
+export default function passportKakaoLoader() {
+  const kakaoStrategyOptions = {
+    clientID: process.env.KAKAO_CLIENT_ID!,
+    clientSecret: process.env.KAKAO_SECRET_ID!,
+    callbackURL: process.env.KAKAO_CALLBACK_URL!,
+  };
+
+  const verifyKakaoUser = async (
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: (error: any, user?: any, info?: any) => void,
+  ) => {
+    const user = await UserService.findOrCreateUserForProvider({
+      authProvider: 'kakao',
+      authProviderID: profile.id,
+    });
+    console.log(profile);
+    done(null, user);
+  };
+
+  passport.use('kakao', new KakaoStrategy(kakaoStrategyOptions, verifyKakaoUser));
+}
