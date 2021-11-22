780b1eba42ce105e2d6036b59bd45fb2d5941226
beomseok han <bmtoss@naver.com>
2021-11-22 23:31:43 +0900

    Feat: dashboard link component 구현

--- frontend/src/components/cards/DashboardLinkCard/index.tsx
+++ frontend/src/components/cards/DashboardLinkCard/index.tsx
@@ -1,7 +1,30 @@
 import CardCommon from 'src/components/cards/Common';
+import { Row, Col } from 'src/components/Grid';
+
+import { Title, Content } from './style';
 
 function DashBoardLinkCard() {
-  return <CardCommon width={400}>링크, 희망 직군?.,,</CardCommon>;
+  return (
+    <CardCommon width={400}>
+      <Row padding={24}>
+        <Col>
+          <Title>희망 직군</Title>
+          <Title>GitHub</Title>
+          <Title>Blog</Title>
+        </Col>
+        <Col>
+          <p>:</p>
+          <p>:</p>
+          <p>:</p>
+        </Col>
+        <Col>
+          <Content>놀고 먹는 개발자</Content>
+          <Content>https://github.com/example</Content>
+          <Content>링크</Content>
+        </Col>
+      </Row>
+    </CardCommon>
+  );
 }
 
 export default DashBoardLinkCard;
---
+++ frontend/src/components/cards/DashboardLinkCard/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Title = styled.p`
+  margin-right: 8px;
+`;
+
+const Content = styled.p`
+  margin-left: 8px;
+`;
+
+export { Title, Content };
