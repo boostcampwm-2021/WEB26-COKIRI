00ee85f837877efe1ded840f90c32d198e2177c1
Hyunmin Jung <jung@hyunmin.dev>
2021-11-22 21:29:18 +0900

    Feat: 외부 타입 프로토타입 개발 완료

--- frontend/src/components/contents/RepoContent/index.tsx
+++ frontend/src/components/contents/RepoContent/index.tsx
@@ -1,15 +1,43 @@
 import PropTypes from 'prop-types';
+import dompurify from 'dompurify';
+
+import { Row, Col } from 'src/components/Grid';
+
+import { RepoInfoType } from 'src/types';
 
 interface Props {
   content: string;
+  link: string;
+  info: RepoInfoType;
 }
 
-function RepoContent({ content }: Props) {
-  return <>todo</>;
+function RepoContent({ content, link, info }: Props) {
+  const sanitizer = dompurify.sanitize;
+  return (
+    <>
+      <Row justifyContent='space-between'>
+        <p>별 {info.starCount}</p>
+        <p>포크 {info.forkCount}</p>
+        {Object.keys(info.language).map((language) => (
+          <Col>
+            <p>{language}</p>
+            <p>{info.language[language]}%</p>
+          </Col>
+        ))}
+      </Row>
+      {/* eslint-disable-next-line react/no-danger */}
+      <div dangerouslySetInnerHTML={{ __html: sanitizer(content) }} />
+      <a target='_blank' href={link} rel='noreferrer noopener'>
+        글 보러가기
+      </a>
+    </>
+  );
 }
 
 RepoContent.propTypes = {
   content: PropTypes.string.isRequired,
+  info: PropTypes.objectOf(PropTypes.any).isRequired,
+  link: PropTypes.string.isRequired,
 };
 
 export default RepoContent;
