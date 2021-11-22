d377a14e8ba48f29e8a665512acd7bdeab06ec4a
beomseok han <bmtoss@naver.com>
2021-11-22 23:30:29 +0900

    Feat: dashboard history component 구현

--- frontend/src/components/cards/DashboardHistoryCard/index.tsx
+++ frontend/src/components/cards/DashboardHistoryCard/index.tsx
@@ -1,7 +1,20 @@
 import CardCommon from 'src/components/cards/Common';
 
+import { History, HorizentalLine } from './style';
+
 function DashboardHistoryCard() {
-  return <CardCommon width={400}>히스토리</CardCommon>;
+  return (
+    <CardCommon width={400}>
+      <History>
+        <p>XXXX.XX.XX</p>
+        <HorizentalLine />
+        <p>example history</p>
+        <p>XXXX.XX.XX</p>
+        <HorizentalLine />
+        <p>example history</p>
+      </History>
+    </CardCommon>
+  );
 }
 
 export default DashboardHistoryCard;
---
+++ frontend/src/components/cards/DashboardHistoryCard/style.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const History = styled.div`
+  margin-left: 50px;
+  border-left: 2px solid #ffffff;
+  p {
+    padding: 8px;
+  }
+`;
+
+const HorizentalLine = styled.div`
+  width: 70%;
+  border-bottom: 2px solid #ffffff;
+`;
+
+export { History, HorizentalLine };
