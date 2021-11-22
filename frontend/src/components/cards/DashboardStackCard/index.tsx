2fadc6797ff84646f6a3eae9e229b40b771d9a05
beomseok han <bmtoss@naver.com>
2021-11-22 23:31:06 +0900

    Feat: dashboard stack component 구현

--- frontend/src/components/cards/DashboardStackCard/index.tsx
+++ frontend/src/components/cards/DashboardStackCard/index.tsx
@@ -1,7 +1,32 @@
 import CardCommon from 'src/components/cards/Common';
 
+import { Col, Row } from 'src/components/Grid';
+
+import { Title, Stack } from './style';
+
 function DashBoardStackCard() {
-  return <CardCommon width={700}>스택</CardCommon>;
+  return (
+    <CardCommon width={700}>
+      <Col padding={8}>
+        <Row padding={8}>
+          <Col>
+            <Title>프론트엔드</Title>
+            <Row>
+              <Stack>React</Stack>
+            </Row>
+          </Col>
+        </Row>
+        <Row padding={8}>
+          <Col>
+            <Title>백엔드</Title>
+            <Row>
+              <Stack>Express</Stack>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+    </CardCommon>
+  );
 }
 
 export default DashBoardStackCard;
---
+++ frontend/src/components/cards/DashboardStackCard/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Title = styled.p`
+  font-size: 16px;
+`;
+
+const Stack = styled.p``;
+
+export { Title, Stack };
