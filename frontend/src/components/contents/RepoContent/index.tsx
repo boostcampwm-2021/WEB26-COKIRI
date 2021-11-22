b09fef8ffc0e5e9dc09e576c9278dbe9fa17c5e5
Hyunmin Jung <jung@hyunmin.dev>
2021-11-22 20:40:08 +0900

    Feat: 외부 컨텐츠 컴포넌트 프로토타입 개발

--- frontend/src/components/contents/RepoContent/index.tsx
+++ frontend/src/components/contents/RepoContent/index.tsx
@@ -1,28 +1,15 @@
 import PropTypes from 'prop-types';
 
-import ContentCommon from 'src/components/contents/Common';
-
-import { DEFAULT_POST_CONTENT_WIDTH } from 'src/globals/constants';
-
 interface Props {
   content: string;
-  width?: number;
-  expanded?: boolean;
 }
 
-function RepoContent({ content, width, expanded }: Props) {
-  return <ContentCommon content={content} width={width} expanded={expanded} />;
+function RepoContent({ content }: Props) {
+  return <>todo</>;
 }
 
 RepoContent.propTypes = {
   content: PropTypes.string.isRequired,
-  width: PropTypes.number,
-  expanded: PropTypes.bool,
-};
-
-RepoContent.defaultProps = {
-  width: DEFAULT_POST_CONTENT_WIDTH,
-  expanded: false,
 };
 
 export default RepoContent;
