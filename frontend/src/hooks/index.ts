bd287969a76717ba6737a63d4639b7c21e8af1e9
Hyunmin Jung <jung@hyunmin.dev>
2021-11-25 17:57:18 +0900

    Refactor: 커스텀 훅 파일 이름 변경

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,4 +1,4 @@
-import useIntersectionObserver from 'src/hooks/IntersectionObserver';
+import useIntersectionObserver from 'src/hooks/intersectionObserver';
 
 // eslint-disable-next-line import/prefer-default-export
 export { useIntersectionObserver };
