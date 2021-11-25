507ca5299a9119c16ca42e7ea993c2db16104bae
Hyunmin Jung <jung@hyunmin.dev>
2021-11-25 17:26:43 +0900

    Feat: 회원가입에 유효성 검사 추가

--- frontend/src/components/modals/RegisterModal/index.tsx
+++ frontend/src/components/modals/RegisterModal/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { useMutation } from 'react-query';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import { AiOutlineCheck } from 'react-icons/ai';
 
 import ModalCommon from 'src/components/modals/Common';
 import InputCommon from 'src/components/inputs/Common';
@@ -10,25 +11,51 @@ import { Fetcher } from 'src/utils';
 
 import userAtom, { isAuthenticatedSelector, isRegisteredSelector } from 'src/recoil/user';
 
+const validateUsername = (username: string) => /^[a-zA-Z0-9_-]{1,20}/.test(username);
+
 function RegisterModal() {
   const [user, setUser] = useRecoilState(userAtom);
   const isRegistered = useRecoilValue(isRegisteredSelector);
   const isAuthenticated = useRecoilValue(isAuthenticatedSelector);
+
   const [username, setUsername] = useState('');
-  const putUserSettings = () => Fetcher.putUserSettings(user, { username });
-  const mutation = useMutation(putUserSettings, {
+  const [isValid, setIsValid] = useState(false);
+
+  const mutation = useMutation(() => Fetcher.putUserSettings(user, { username }), {
     onSuccess: () => setUser({ ...user, isRegistered: true, username }),
   });
+  const validateMutation = useMutation(
+    (newUsername: string) => Fetcher.getIsExistUsername(newUsername),
+    { onSuccess: (data) => setIsValid(!data) },
+  );
+
   const handleConfirm = useCallback(() => mutation.mutate(), [mutation]);
   if (!isAuthenticated || isRegistered) {
     return null;
   }
+
+  const handleChange = (value: string) => {
+    if (validateUsername(value)) {
+      validateMutation.mutate(value);
+    } else {
+      setIsValid(false);
+    }
+  };
   return (
-    <ModalCommon onConfirm={handleConfirm} close='로그아웃' confirm='확인'>
+    <ModalCommon
+      onConfirm={handleConfirm}
+      close='로그아웃'
+      confirm='확인'
+      title='username 입력'
+      disabled={!isValid}
+    >
       <Col alignItems='center'>
-        <p>회원가입에 필요한 절차에요</p>
-        <p>username을 알려주세요~</p>
-        <InputCommon bind={[username, setUsername]} />
+        <p>영문 대소문자 및 숫자만 가능해요</p>
+        <InputCommon
+          bind={[username, setUsername]}
+          onChange={handleChange}
+          icon={isValid ? <AiOutlineCheck /> : null}
+        />
       </Col>
     </ModalCommon>
   );
