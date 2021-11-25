a826f211eac3ccb94716b37439169106b855ce92
Hyunmin Jung <jung@hyunmin.dev>
2021-11-25 18:39:10 +0900

    Feat: 디바운싱 커스텀 훅 구현

---
+++ frontend/src/hooks/debounce.ts
@@ -0,0 +1,15 @@
+import { DependencyList, useEffect, useRef } from 'react';
+
+const useDebounce = (callback: () => void, delay: number, deps: DependencyList) => {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+  useEffect(() => {
+    if (+timer.current! !== 0) {
+      clearTimeout(timer.current!);
+    }
+    timer.current = setTimeout(() => {
+      callback();
+    }, delay);
+  }, deps);
+};
+
+export default useDebounce;
