35ea363d533ecd0ddcdb1b0844332d127a490bea
Hyunmin Jung <jung@hyunmin.dev>
2021-11-25 12:21:55 +0900

    Style: 글 작성 모달 스타일에서 이미지 버튼 정상적으로 변경

--- frontend/src/components/modals/PostWriteModal/style.ts
+++ frontend/src/components/modals/PostWriteModal/style.ts
@@ -17,11 +17,13 @@ const IconHolder = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #1e1e1e;
-  box-shadow: 5px 5px 10px #1a1a1a, -5px -5px 10px #232323;
   align-self: start;
   width: 48px;
   height: 48px;
+  border-radius: 12px;
+  background: #444444;
+  box-shadow: 11px 11px 21px #3a3a3a, -11px -11px 21px #4e4e4e;
+  margin-right: 12px;
 `;
 
 export { Textarea, IconHolder };
