daede136e1f8b1fb8989fafb892f01b1990133bc
beomseok han <bmtoss@naver.com>
2021-11-27 23:29:55 +0900

    Feat: 대시보드 레포 추가 버튼 구현

--- frontend/src/components/modals/DashboardRepoAddModal/index.tsx
+++ frontend/src/components/modals/DashboardRepoAddModal/index.tsx
@@ -1,14 +1,96 @@
+import { useState } from 'react';
+import { useMutation, useQuery } from 'react-query';
+import { useRecoilValue } from 'recoil';
+
 import ModalCommon from 'src/components/modals/Common';
+import ButtonCommon from 'src/components/buttons/Common';
+import ExternalPreview from 'src/components/ExternalPreview';
+import NavigateIconButton from 'src/components/buttons/NavigateIconButton';
+import { Col } from 'src/components/Grid';
+
+import userAtom from 'src/recoil/user';
+
+import { DashboardRepoType, ExternalType } from 'src/types';
+
+import { Fetcher } from 'src/utils';
+
+import { Repos } from './style';
 
 interface Props {
   onClose: () => void;
+  // eslint-disable-next-line no-unused-vars
+  onAddRepo: (repo: DashboardRepoType) => void;
 }
 
-function DashboardRepoAddModal({ onClose }: Props) {
-  const onHistoryWrite = () => {};
+function DashboardRepoAddModal({ onClose, onAddRepo }: Props) {
+  const user = useRecoilValue(userAtom);
+  const { hasExternalGithub } = user;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [external, setExternal] = useState<ExternalType>();
+  const { data: repos } = useQuery(['dashboard', 'repoList', user._id], () =>
+    Fetcher.getUserRepos(user),
+  );
+
+  const getRepoMutation = useMutation((name: string) => Fetcher.getUserRepo(user, name), {
+    onSuccess: (repo: ExternalType) => {
+      setExternal(repo);
+    },
+  });
+
+  const postRepoMutation = useMutation(
+    (repoName: string) => Fetcher.postDashboardRepo(user, repoName),
+    {
+      onSuccess: (data) => onAddRepo(data),
+    },
+  );
+
+  const handleConfirm = () => {
+    postRepoMutation.mutate(repos![selectedIndex].name);
+    onClose();
+  };
+
+  const handleRepoClick = (index: number) => {
+    if (index === selectedIndex) {
+      setSelectedIndex(-1);
+    } else {
+      getRepoMutation.mutate(repos![index].name);
+      setSelectedIndex(index);
+    }
+  };
+
+  const handleExternalDelete = () => {
+    setExternal(undefined);
+    setSelectedIndex(-1);
+  };
+
   return (
-    <ModalCommon onConfirm={onHistoryWrite} onClose={onClose} confirm='저장' close='취소'>
-      레포 추가
+    <ModalCommon
+      onConfirm={handleConfirm}
+      onClose={onClose}
+      confirm='저장'
+      close='취소'
+      title={hasExternalGithub ? '저장소를 선택하세요' : '연동된 깃헙이 없습니다'}
+      disabled={selectedIndex === -1}
+    >
+      {hasExternalGithub ? (
+        <Repos>
+          {(repos ?? []).map((repo, index) => (
+            <Col key={repo.name}>
+              <ButtonCommon
+                onClick={() => handleRepoClick(index)}
+                clicked={index === selectedIndex}
+              >
+                {repo.name}
+              </ButtonCommon>
+            </Col>
+          ))}
+        </Repos>
+      ) : (
+        <NavigateIconButton href={`/users/${user.username}/settings`}>
+          연동하러 가기
+        </NavigateIconButton>
+      )}
+      <ExternalPreview external={external} onDelete={handleExternalDelete} />
     </ModalCommon>
   );
 }
---
+++ frontend/src/components/modals/DashboardRepoAddModal/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  position: absolute;
+  z-index: 3;
+`;
+
+const Repos = styled.div`
+  overflow-y: auto;
+  height: 400px;
+`;
+
+export { Wrapper, Repos };
