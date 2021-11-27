4be2fcf1e2214ba7a2675811cb000ee22049313f
Hyunmin Jung <jung@hyunmin.dev>
2021-11-27 15:23:07 +0900

    Feat: 헤더 및 회원가입 모달에 로그아웃 버튼 추가

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,11 +1,13 @@
 import { useRecoilValue } from 'recoil';
 import PropTypes from 'prop-types';
+import { AiOutlineLogout } from 'react-icons/ai';
 import { IoHomeOutline, IoCompassOutline, IoPersonCircleOutline } from 'react-icons/io5';
 
 import NavigateIconButton from 'src/components/buttons/NavigateIconButton';
 import LogoButton from 'src/components/buttons/LogoButton';
 import UserSearchInput from 'src/components/inputs/UserSearchInput';
 import NotificationButton from 'src/components/buttons/NotificationButton';
+import IconButton from 'src/components/buttons/IconButton';
 
 import userAtom, { isAuthenticatedSelector } from 'src/recoil/user';
 
@@ -15,9 +17,12 @@ interface Props {
   page?: string;
 }
 
+const url = process.env.NEXT_PUBLIC_SERVER_URL as string;
+
 function Header({ page }: Props) {
   const user = useRecoilValue(userAtom);
   const isAuthenticated = useRecoilValue(isAuthenticatedSelector);
+  const handleLogoutClick = () => window.open(`${url}/v1/users/logout`, '_self');
 
   return (
     <Wrapper>
@@ -42,6 +47,9 @@ function Header({ page }: Props) {
             <NavigateIconButton href={`/users/${user.username}`} clicked={page === 'username'}>
               <IoPersonCircleOutline />
             </NavigateIconButton>
+            <IconButton onClick={handleLogoutClick}>
+              <AiOutlineLogout />
+            </IconButton>
           </>
         )}
       </Section>
