44e679d7db68176faa32b36854716b52007182c4
beomseok han <bmtoss@naver.com>
2021-11-29 23:32:49 +0900

    Chore: declare 함수에 .env 타입 지정

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,6 +1,14 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SERVER_URL: string;
+    NEXT_PUBLIC_STORAGE_BUCKET: string;
+    NEXT_PUBLIC_STORAGE_END_POINT: string;
+    NEXT_PUBLIC_STORAGE_ACCESS_KEY: string;
+    NEXT_PUBLIC_STORAGE_SECRET_KEY: string;
+  }
+}
