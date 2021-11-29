075ec8425eff5dbb45a696bf03b8b7627d7516c5
Dongmin Kim <kdmin0211@gmail.com>
2021-11-29 17:10:49 +0900

    Refactor: 시나리오 테스트 로직 변경

--- backend/src/test/users.test.ts
+++ backend/src/test/users.test.ts
@@ -9,9 +9,6 @@ import { UserType } from 'src/types';
 beforeAll(async () => {
   await connect();
 });
-afterEach(async () => {
-  await dropDatabase();
-});
 afterAll(async () => {
   await disconnect();
 });
@@ -31,6 +28,10 @@ describe('회원가입', () => {
     await User.create(user);
   });
 
+  afterAll(async () => {
+    await dropDatabase();
+  });
+
   test('자신의 정보 요청', async () => {
     const response = await request(app).get('/v1/users/me').set('Authorization', `Bearer ${token}`);
     user = response.body.data;
@@ -40,20 +41,28 @@ describe('회원가입', () => {
   });
 
   test('유저네임 유효성 검사', async () => {
-    const responseTrue = await request(app).get('/v1/users?query=test1');
-    expect(responseTrue.body.data).toBe(false);
+    const responseTrue = await request(app).get('/v1/users?query=test');
+    expect(responseTrue.body.data).toBe(true);
     expect(responseTrue.status).toBe(200);
 
-    const responseFalse = await request(app).get('/v1/users?query=test1');
+    const responseFalse = await request(app).get('/v1/users?query=testuser');
     expect(responseFalse.body.data).toBe(false);
     expect(responseFalse.status).toBe(200);
+  });
+
+  test('사용자 입력 데이터 저장(회원가입)', async () => {
+    const response = await request(app)
+      .put(`/v1/users/${user._id}/settings`)
+      .send({ name: 'testUser', username: 'test1' })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(401);
   });
 });
 
 describe('타입라인 테스트', () => {
   test('타임라인 불러오기', async () => {
     const response = await request(app).get('/v1/users/me').set('Authorization', `Bearer ${token}`);
-    expect(response.body.code).toBe(RESPONSECODE.SUCCESS);
     expect(response.status).toBe(200);
   });
 });
