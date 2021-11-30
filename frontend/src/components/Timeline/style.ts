db08bcb9bab0e9b1e72de500f5e333f683135521
Hyunmin Jung <jung@hyunmin.dev>
2021-12-01 02:02:33 +0900

    Feat: 타임라인 react-virtualized로 최적화

---
+++ frontend/src/components/Timeline/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Observer = styled.div`
+  margin-bottom: 8px;
+`;
+
+const EndLabel = styled.p`
+  margin-bottom: 16px;
+`;
+
+export { Observer, EndLabel };
